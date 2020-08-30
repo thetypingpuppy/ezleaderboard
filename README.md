09fd48c4da5d185eb0e4467c1e50f1533d3911f0
Craig Brown <craig.robert.brown@gmail.com>
2020-08-30 21:40:54 +0100

    Basics working.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # ezleaderboard
+
+Install with the following:
+
+```console
+cordova plugin add ezleaderboard --value PLAY_APP_ID=123456789012 --value PLAY_LEADERBOARD_ID=CgkPpDzMxOAWEAIQAQ
+'''
+
+Submit a score of 1401:
+
+```javascript
+        cordova.exec(
+            function(winParam) {},
+            function(error) {},
+            "ezleaderboard",
+            "submitToLeaderboard",
+            [1401]);
+'''
--- src/android/ezleaderboard.java
+++ src/android/ezleaderboard.java
@@ -1,6 +1,9 @@
 package com.orbost.plugins;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
 import android.view.WindowManager;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -21,104 +24,74 @@ import com.google.android.gms.tasks.Task;
 import com.orbost.bananareach.R;
 
 public class ezleaderboard extends CordovaPlugin {
+private static final String ACTION_SUBMIT_TO_LEADERBOARD = "submitToLeaderboard";
+public int score;
 
-  private static final String ACTION_SUBMIT_TO_LEADERBOARD = "submitToLeaderboard";
-  private static final String ACTION_ALLOW_SLEEP_AGAIN = "allowSleepAgain";
-
-  @Override
-  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-    try {
+@Override
+public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+  try {
       if (ACTION_SUBMIT_TO_LEADERBOARD.equals(action)) {
 
-        Context context = this.cordova.getActivity().getApplicationContext();
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN).requestEmail().build();
-        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(context, gso);
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        this.cordova.getActivity().startActivityForResult(signInIntent, 1);
+          score = args.getInt(0);
 
-        //return true;
+          Context context = this.cordova.getActivity().getApplicationContext();
+          GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN).requestEmail().build();
+          GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(context, gso);
+          Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+          this.cordova.setActivityResultCallback (this);
+          this.cordova.getActivity().startActivityForResult(signInIntent, 1);
 
-
-        // cordova.getActivity().runOnUiThread(
-        //     new Runnable() {
-        //       public void run() {
-        //         cordova.getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        //         callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK));
-        //       }
-        //     });
-
-
-      } else if (ACTION_ALLOW_SLEEP_AGAIN.equals(action)) {
-        cordova.getActivity().runOnUiThread(
-                new Runnable() {
-                  public void run() {
-                    cordova.getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK));
-                  }
-                });
-        return true;
-
-      } else {
-        callbackContext.error("ezleaderboard." + action + " is not a supported function. Did you mean '" + ACTION_SUBMIT_TO_LEADERBOARD + "'?");
-        return false;
-      }
-    } catch (Exception e) {
-      callbackContext.error(e.getMessage());
+    }
+    else {
+      
+      callbackContext.error("ezleaderboard." + action + " is not a supported function. Did you mean '" + ACTION_SUBMIT_TO_LEADERBOARD + "'?");
       return false;
-    }
 
-  return true;
+    }
+  } catch (Exception e) {
+
+    callbackContext.error(e.getMessage());
+    return false;
+
   }
 
-  @Override
-  public void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
+return true;
 
-    // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-    if (requestCode == 1) {
-      // The Task returned from this call is always completed, no need to attach
-      // a listener.
-      Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-      handleSignInResult(task);
-    }
+}
 
-    if (requestCode == 2) {
+@Override
+public void onActivityResult(int requestCode, int resultCode, Intent data) {
+  super.onActivityResult(requestCode, resultCode, data);
 
-    }
+
+  if (requestCode == 1) {
+    Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+    this.handleSignInResult(task);
   }
+}
 
-
-  private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-    GoogleSignInAccount account = null;//GoogleSignIn.getLastSignedInAccount(this);
-    try {
+private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+  GoogleSignInAccount account = null;//GoogleSignIn.getLastSignedInAccount(this);
+  try {
       account = completedTask.getResult(ApiException.class);
-      //account.
 
+  } catch (ApiException e) {
 
-
-      // Signed in successfully, show authenticated UI.
-      //updateUI(account);
-    } catch (ApiException e) {
-      // The ApiException status code indicates the detailed failure reason.
-      // Please refer to the GoogleSignInStatusCodes class reference for more information.
-
+      Log.w("TAG", "signInResult:failed code=" + e.getStatusCode());
       return;
-      //Log.w("TAG", "signInResult:failed code=" + e.getStatusCode());
-      //updateUI(null);
-    }
-
-    assert account != null;
-    Games.getLeaderboardsClient(this.cordova.getActivity(), account).submitScore(this.cordova.getActivity().getString(R.string.ezleaderboard_leaderboard_id), 1400);
-
-    Games.getLeaderboardsClient(this.cordova.getActivity(), GoogleSignIn.getLastSignedInAccount(this.cordova.getActivity().getApplicationContext()))
-            .getLeaderboardIntent(this.cordova.getActivity().getString(R.string.ezleaderboard_leaderboard_id))
-            .addOnSuccessListener(new OnSuccessListener<Intent>() {
-              @Override
-              public void onSuccess(Intent intent) {
-                cordova.getActivity().startActivityForResult(intent, 2);
-              }
-            });
-
+    //updateUI(null);
   }
 
+  assert account != null;
+  Games.getLeaderboardsClient(this.cordova.getActivity(), account).submitScore(this.cordova.getActivity().getString(R.string.ezleaderboard_leaderboard_id), score);
+
+  Games.getLeaderboardsClient(this.cordova.getActivity(), GoogleSignIn.getLastSignedInAccount(this.cordova.getActivity().getApplicationContext()))
+          .getLeaderboardIntent(this.cordova.getActivity().getString(R.string.ezleaderboard_leaderboard_id))
+          .addOnSuccessListener(new OnSuccessListener<Intent>() {
+            @Override
+            public void onSuccess(Intent intent) {
+              cordova.getActivity().startActivityForResult(intent, 2);
+            }
+          });
+}
 }
